77eb6e222547ef1f096a675d84a0c01fcf73a16a
Sabdar <sabdarshaikh10@gmail.com>
2024-05-04 16:50:26 +0530

    separate routes for user and blog

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,16 @@
 import { Hono } from 'hono'
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
+import { cors } from 'hono/cors'
 
-// Create the main Hono app
-const app = new Hono()
-
-app.post('/api/v1/signup', (c) => {
-  return c.text('signup route')
-})
-
-app.post('/api/v1/signin', (c) => {
-  return c.text('signin route')
-})
-
-
-
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
-
-app.post('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
-
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>();
+app.use('/*', cors())
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
 export default app
